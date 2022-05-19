f06fd9e2b193a394db654935b0f63a21ce4ad682
Mariem Slimene <mariem.slimene7797@gmail.com>
2022-05-19 18:24:11 +0100

    Updated code style

--- src/AppLayout/LayoutContent/index.tsx
+++ src/AppLayout/LayoutContent/index.tsx
@@ -1,12 +1,16 @@
 import { Layout } from 'antd';
+import Sider from 'antd/lib/layout/Sider';
 import React from 'react';
 
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
 interface LayoutContentProps {}
 
 const LayoutContent: React.FC<LayoutContentProps> = () => {
-
-      return <Layout.Content style={{width:"100%", height:"100%"}}>Content</Layout.Content>;
+  return (
+    <>
+      <Layout.Content style={{ width: '100%', height: '100%' }} />
+    </>
+  );
 };
 
 export default LayoutContent;
-
