a8215845bd3871e7d6eda8cd0e07d9beb4813405
Bilel Haddaji <haddaji94bilel@gmail.com>
2022-05-23 14:39:21 +0100

    add react grid layout to project

--- README.md
+++ README.md
@@ -44,3 +44,10 @@ You don’t have to ever use `eject`. The curated feature set is suitable for sm
 You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
 
 To learn React, check out the [React documentation](https://reactjs.org/).
+
+## to do
+
+- change all inline styles with React.CSSProperties
+- create DashBoard Toolbox
+- create Dashboard Item
+- Create Toolbox Item
--- src/pages/Dashboard/Sider/SiderItem/index.tsx
+++ src/pages/Dashboard/Sider/SiderItem/index.tsx
@@ -6,10 +6,12 @@ interface SiderItemProps {}
 
 const SiderItem: React.FC<SiderItemProps> = () => {
   return (
-    <Card hoverable={true} style={{ width: 250, borderColor: 'blueviolet', margin: 20, height: 70 }}>
+    <Card hoverable={true} style={CardStyle}>
       <p style={{ textAlign: 'center' }}>Card Content</p>
     </Card>
   );
 };
 
 export default SiderItem;
+
+const CardStyle: React.CSSProperties = { width: 250, borderColor: 'blueviolet', margin: 20, height: 70 };
