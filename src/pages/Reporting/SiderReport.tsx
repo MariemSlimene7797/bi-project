970a351794d2ec52cacf2855d8c2dd2f953dadb2
Mariem Slimene <mariem.slimene7797@gmail.com>
2022-06-21 18:13:40 +0100

    ReportSider (favorite button)

--- src/pages/Reporting/SiderReport.tsx
+++ src/pages/Reporting/SiderReport.tsx
@@ -2,7 +2,7 @@ import { Layout, Menu } from 'antd';
 import React from 'react';
 import { useLayoutContext } from '../../contexts/LayoutContext';
 import { ItemType } from 'antd/lib/menu/hooks/useItems';
-import { useThemeContext } from '../../contexts/ThemeContext';
+import { StarFilled, StarOutlined } from '@ant-design/icons';
 /**Creation of the collapsed sider and its items along with their options */
 /**creation of the record */
 const groupBy = <T, K extends keyof any>(arr: T[], key: (i: T) => K) =>
@@ -24,28 +24,46 @@ const siderData: siderDataType[] = [
   { id: 2, name: 'Report 3', category: 'Direction Generale' },
   { id: 3, name: 'Report 4', category: 'CRM' }
 ];
+const FavList: siderDataType[] = [];
+//
+
 /**the FC that inserted the items of the Report Sider */
 const SiderReport: React.FC = () => {
   /**the collapsed button */
   const { collapsed } = useLayoutContext();
   /**list of the categories */
   const results = groupBy(siderData, (i) => i.category);
+
   const categories = Object.keys(results);
+
+  categories.unshift('Favorites');
+  console.log(categories);
+
   /**creation of the menu Items  */
+
   const menuItems: ItemType[] = categories.map((category) => {
     /**inserting the list of reports of each category in the sider */
+
     const reports = siderData.filter((item) => item.category === category);
+    console.log('reports', reports);
     return {
       key: category,
       label: category,
+      icon: <></>,
       children: reports.map((report) => {
-        return { key: report.id, label: report.name };
+        if (category === 'Favorites') {
+          return { key: report.id, label: report.name, icon: <StarFilled /> };
+        } else {
+          return { key: report.id, label: report.name, icon: <StarOutlined /> };
+        }
       })
     };
   });
+
   return (
     <Layout.Sider style={siderStyle} theme="light" trigger={null} collapsible collapsed={collapsed}>
       {console.log('items', menuItems)}
+
       <Menu theme="light" mode="inline" items={menuItems} style={SiderItemStyle} />
     </Layout.Sider>
   );
