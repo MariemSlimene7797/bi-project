ae5498b5aca11136150eac41cf43bc22872c2892
Mariem Slimene <mariem.slimene7797@gmail.com>
2022-08-30 16:43:53 +0100

    Reporting Sider (category list and their reports)

---
+++ src/pages/Reporting/FormReportCreation.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+interface FormReportCreationProps {}
+
+const FormReportCreation: React.FC<FormReportCreationProps> = () => {
+  return <>FormReportCreation</>;
+};
+
+export default FormReportCreation;
