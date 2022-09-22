538dbdfe768380d678fa325d06d4747d6c98af67
Mariem Slimene <mariem.slimene7797@gmail.com>
2022-09-22 16:28:21 +0100

    code

---
+++ src/pages/Reporting/ReportViewerModule.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+//Report designer source
+import '@boldreports/javascript-reporting-controls/Scripts/bold.report-viewer.min';
+import '@boldreports/javascript-reporting-controls/Content/material/bold.reports.all.min.css';
+//Data-Visualization
+import '@boldreports/javascript-reporting-controls/Scripts/data-visualization/ej.bulletgraph.min';
+import '@boldreports/javascript-reporting-controls/Scripts/data-visualization/ej.chart.min';
+//Reports react base
+import '@boldreports/react-reporting-components/Scripts/bold.reports.react.min';
+declare let BoldReportViewerComponent: any;
+
+const viewerStyle = {
+  height: '700px',
+  width: '100%'
+};
+const ReportViewerModule: React.FC = () => {
+  const reportServiceUrl = 'https://localhost:7215/api/ReportViewer';
+  // https://help.boldreports.com/report-viewer-sdk/faq/difference-between-report-service-url-and-report-server-url/
+  // https://help.boldreports.com/report-viewer-sdk/javascript-reporting/report-viewer/report-service/create-aspnet-core-web-api-service/
+  const reportServerUrl = 'http://desktop-3d2n79g/ReportServer2022'; //ok
+  const reportPath = '~/Resources/test.rdl';
+  return (
+    <BoldReportViewerComponent
+      id="reportviewer-container"
+      reportServiceUrl={reportServiceUrl}
+      reportPath={reportPath}
+      //http://desktop-3d2n79g/Reports2022/report/test
+      reportServerUrl={reportServerUrl}
+    ></BoldReportViewerComponent>
+  );
+};
+
+export default ReportViewerModule;
